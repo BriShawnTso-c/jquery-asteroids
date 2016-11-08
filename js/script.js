6402d465af06f121d1d79760da5783550ae7ffd3
BriShawnTso-c <bsyazzie@hotmail.com>
2016-11-08 10:43:07 -0700

    added js script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY"
+var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=yCLoV0xsKEfSdPUZcfAFEJlNY2QcuuChQ7FrzQCf"
 
 $.ajax({
   url: url,
